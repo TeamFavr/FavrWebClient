d7aef47e20e1bc918b6a2a43df92ee94570a4396
emilyperegrine <peregrineemily@gmail.com>
2016-08-08 02:49:04 +0100

    Added flat_ui free

--- app/static/src/index.js
+++ app/static/src/index.js
@@ -2,8 +2,13 @@ module.exports = function () {
   var App = require("./components/App.vue")
   var Vue = require("Vue");
   var $ = require("jquery");
+
   window.$ = $;
   window.jQuery = $;
+
+  //flat_ui.js not working
+  //var flat_ui = require("./flat-ui")
+  //var flui_vid = require("./video")
 
   $(document).ready(function () {
     new Vue({
