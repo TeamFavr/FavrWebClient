652f8a9c848ff1e34171d6e6188f8cfd41e306b0
emilyperegrine <peregrineemily@gmail.com>
2016-08-08 10:30:07 +0100

    Made landing carousel

---
+++ app/static/src/components/LandingCarousel.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+      <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="http://placehold.it/1280x720">
+        <div class="carousel-caption">
+          <h3>Caption 1</h3>
+          <p>caption description</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="http://placehold.it/1280x720">
+        <div class="carousel-caption">
+          <h3>Caption 2</h3>
+          <p>caption description</p>
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="http://placehold.it/1280x720">
+        <div class="carousel-caption">
+          <h3>Caption 3</h3>
+          <p>caption description</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</template>
--- app/static/src/index.js
+++ app/static/src/index.js
@@ -1,5 +1,5 @@
 module.exports = function () {
-  var App = require("./components/App.vue")
+  var LandingCarousel = require("./components/LandingCarousel.vue")
   var Vue = require("Vue");
   var jq = require("jquery");
 
@@ -10,7 +10,7 @@ module.exports = function () {
     new Vue({
       el: 'body',
       components: {
-        app: App
+        "landing-carousel": LandingCarousel
       }
     });
   });
