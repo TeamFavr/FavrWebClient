a8e607f6dac64c48e85450a1fe88256a05d43877
emilyperegrine <peregrineemily@gmail.com>
2016-08-11 23:19:18 +0100

    Added api_url environment variable

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 app = Flask("app", static_folder="static/dist")
+import os
 
 DEBUG = True
 PORT = 8000
 HOST = '0.0.0.0'
+
+@app.before_request
+def before_req():
+    g.api_url = os.environ['API_URL']
 
 @app.route("/")
 def landing():
@@ -12,6 +17,10 @@ def landing():
 @app.route("/home")
 def home():
     return render_template("home.jinja")
+
+@app.route("/config/url")
+def config_url():
+    return g.api_url
 
 if __name__ == "__main__":
     app.run(debug=DEBUG, host=HOST, port=PORT)
